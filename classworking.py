b0d0d486619125b36443ac527aafef27027c5593
carryfan-hub <sademuditraj4422@gmail.com>
2020-06-21 23:45:13 +0300

    classworking.py

--- classworking.py
+++ classworking.py
@@ -1,38 +1,46 @@
 #praise the lord,jesus,christ
 #lord jesus christplease help me
 
-m="school"
-class student:
-    
-    m="school"
+import camelcase
+c = camelcase.CamelCase()
+x = str(input())
+t=0
+m=0
+s=[]
+for i in x:
+    try:
+        v=int(i)
+        t+=1
+    except:
+        m+=1
+        s.append(i)
 
-    
-    def _init_(self,name,age,rollnumber):
-        self.name=name
-        self.age = age
-        self.rollnumber=rollnumber
-        print("hello")
-    def temporary_method(self):
-        print("bye")
-    @classmethod
-    def info(self):
-        
-        print(self.m)
-    @staticmethod
-    def rodo(w):
-        print("hello")
 
-    
-w=student()
-w.name = "mudit"
-w.age = "34"
-w.rollnumber="45"
-w._init_("mudit",34,56789)
-s=student()
-s._init_("frodo",34,56)
-s.temporary_method()
-w.temporary_method()
-w.info()
+print(t)
+print(m)
+st="".join(s)
+print(st)
+print(s)
+o=0
+q=0
+op=0
+for us in st:
+    if c.hump(us)==s[op]:
+        o+=1
+    else:
+        q+=1
+    op+=1
+    if op == len(s):
+        break
+
+print(o)
+print(q)
+
+
+
+
+
+
 
 
   

af95a869d1f1a4b7e173af016a6474bd3ee02b4d
carryfanhub <sademuditraj4422@gmail.com>
2020-06-23 23:09:53 +0300

    client.py

--- client.py
+++ client.py
@@ -1,7 +1,17 @@
 #praise the lord,jesus,christ
 #lord jesus christ please help me
-import socket
-d = socket.socket()
-d.connect(("localhost",9999))
-while True:
-    d.send(bytes("hello","utf-8"))
+from tkinter import *
+window = Tk()
+j=Entry(window,width=200)
+j.pack()
+def chat():
+    import socket
+    mk = socket.socket()
+    mk.connect(("localhost",9990))
+    l =j.get()
+    mk.send(bytes(l,"utf-8"))
+    
+ki = Button(window,text="go",command = chat)
+ki.pack()
+window.mainloop()
+    
--- server.py
+++ server.py
@@ -1,13 +1,22 @@
 #praise the lord,jesus,christ
 #lord jesus christplease help me
-import socket
-v = socket.socket()
-v.bind(("localhost",9999))
-v.listen(2)
-while True:
+from tkinter import *
+window = Tk()
+x=Entry(window,width=200)
+x.pack()
+def start():
+    import socket
+    v=socket.socket()
+    v.bind(("localhost",9990))
+    v.listen(2)
+    print("praise the lord")
     c,addr = v.accept()
-    m=c.recv(1024).decode()
-    print(m)
-    
-    
-
+    while True:
+        c,addr=v.accept()
+        m = c.recv(1024).decode()
+        print("all ok")
+        x.insert(0,m)
+        c.close()
+g = Button(window,text="press",command=start)
+g.pack()
+window.mainloop()

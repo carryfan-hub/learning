03c4f071fc6075d21bb1e355f1b7b2c977beff19
carryfanhub <sademuditraj4422@gmail.com>
2020-06-23 22:19:23 +0300

    server.py

--- client.py
+++ client.py
@@ -1,19 +1,7 @@
 #praise the lord,jesus,christ
 #lord jesus christ please help me
-from tkinter import *
-window = Tk()
-g=Entry(window,width=200)
-g.focus()
-g.pack()
-def chat():
-    import socket
-    o=socket.socket()
-    o.connect(("localhost",9999))
-    l=g.get()
-    o.send(bytes(str(l),"utf-8"))
-    g.delete(0,END)
-h=Button(window,text="connec to the server",command=chat)
-h.pack()
-
-
-window.mainloop()
+import socket
+d = socket.socket()
+d.connect(("localhost",9999))
+while True:
+    d.send(bytes("hello","utf-8"))
--- server.py
+++ server.py
@@ -1,23 +1,13 @@
 #praise the lord,jesus,christ
 #lord jesus christplease help me
-from tkinter import * 
-window = Tk()
-x=Entry(window,width=200000)
-x.pack()
-def connect():
-    import socket
-    f = socket.socket()
-    f.bind(("localhost",9999))
-    f.listen(2)
-    while True:
-        c,addr = f.accept()
-        x.insert(0,"praise the lord")
-        m=c.recv(2438467587).decode()
-        x.insert(0,str(m))
-        c.close()
-        
+import socket
+v = socket.socket()
+v.bind(("localhost",9999))
+v.listen(2)
+while True:
+    c,addr = v.accept()
+    m=c.recv(1024).decode()
+    print(m)
     
-d=Button(window,text="startchat",command=connect)
-d.pack()
-window.mainloop()
+    
 

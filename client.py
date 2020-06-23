6b12d18a0682e7d52bf023f772bb91248757c023
carryfanhub <sademuditraj4422@gmail.com>
2020-06-23 23:36:48 +0300

    server.py

--- client.py
+++ client.py
@@ -1,17 +1,13 @@
 #praise the lord,jesus,christ
 #lord jesus christ please help me
-from tkinter import *
-window = Tk()
-j=Entry(window,width=200)
-j.pack()
-def chat():
-    import socket
-    mk = socket.socket()
-    mk.connect(("localhost",9990))
-    l =j.get()
-    mk.send(bytes(l,"utf-8"))
-    
-ki = Button(window,text="go",command = chat)
-ki.pack()
-window.mainloop()
+import socket
+f = socket.socket()
+f.connect(("localhost",9990))
+m=str(input())
+y=input()
+k=input()
+f.send(bytes(str(m),"utf-8"))
+f.send(bytes(y,"utf-8"))
+f.send(bytes(k,"utf-8"))
+
     
--- server.py
+++ server.py
@@ -1,22 +1,19 @@
 #praise the lord,jesus,christ
 #lord jesus christplease help me
-from tkinter import *
-window = Tk()
-x=Entry(window,width=200)
-x.pack()
-def start():
-    import socket
-    v=socket.socket()
-    v.bind(("localhost",9990))
-    v.listen(2)
-    print("praise the lord")
-    c,addr = v.accept()
-    while True:
-        c,addr=v.accept()
-        m = c.recv(1024).decode()
-        print("all ok")
-        x.insert(0,m)
-        c.close()
-g = Button(window,text="press",command=start)
-g.pack()
-window.mainloop()
+import socket
+v = socket.socket()
+v.bind(("localhost",9990))
+v.listen(2)
+while True:
+    c,addr=v.accept()
+    print(c,addr)
+    m = c.recv(1024).decode()
+    y = c.recv(1).decode()
+    k = c.recv(1).decode()
+    if m == "subtraction":
+        j = int(y) - int(k)
+        print(j)
+    else:
+        c.send(bytes("no","utf-8"))
+        
+    c.close()

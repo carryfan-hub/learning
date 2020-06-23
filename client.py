cfb2477f86685af5c96cc049b5985a186b738b45
carryfanhub <sademuuditraj4422@gmail.com>
2020-06-23 09:00:26 +0300

    server.py

--- client.py
+++ client.py
@@ -1,8 +1,19 @@
-import socket
-d=socket.socket()
-d.connect(("localhost",9999))
-for t in range(0,2):
-    f = d.recv(24253654).decode()
-    print(f)
+#praise the lord,jesus,christ
+#lord jesus christplease help me
+from tkinter import *
+window = Tk()
+d = Entry(window,width=200)
+d.pack()
+def connect():
+    import socket
+    v=socket.socket()
+    v.connect(("localhost",9999))
+    l=v.recv(425365).decode()
+    d.delete(0,END)
+    d.insert(0,str(l))
+    
+g = Button(window,text="connect",command=connect)
+g.pack()
+window.mainloop()
 
 
--- server.py
+++ server.py
@@ -1,15 +1,15 @@
 #praise the lord,jesus,christ
 #lord jesus christplease help me
 import socket
-s = socket.socket()
-print("praise the lord")
-s.bind(("localhost",9999))
-s.listen(1)
-
+t = socket.socket()
+t.bind(("localhost",9999))
+t.listen(2)
 while True:
-    c,addr = s.accept()
-    print(c,addr)
-    for i in range(0,5):
-        c.send(bytes(str(i),"utf-8"))
+    c,addr = t.accept()
+    c.send(bytes("praise the lord","utf-8"))
     c.close()
+
+
+
+
 

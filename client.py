d9d214711791784b530c15f549975f6b390da555
carryfanhub <sademuditraj4422@gmail.com>
2020-06-24 23:29:26 +0300

    client.py

--- client.py
+++ client.py
@@ -1,13 +1,17 @@
 #praise the lord,jesus,christ
 #lord jesus christ please help me
-import socket
-f = socket.socket()
-f.connect(("localhost",9990))
-m=str(input())
-y=input()
-k=input()
-f.send(bytes(str(m),"utf-8"))
-f.send(bytes(y,"utf-8"))
-f.send(bytes(k,"utf-8"))
+from pynput.mouse import Button,Controller
+mouse = Controller()
+
+
+
+
+
+
+
+    
+    
+    
+    
 
     
--- server.py
+++ server.py
@@ -1,19 +1,2 @@
 #praise the lord,jesus,christ
 #lord jesus christplease help me
-import socket
-v = socket.socket()
-v.bind(("localhost",9990))
-v.listen(2)
-while True:
-    c,addr=v.accept()
-    print(c,addr)
-    m = c.recv(1024).decode()
-    y = c.recv(1).decode()
-    k = c.recv(1).decode()
-    if m == "subtraction":
-        j = int(y) - int(k)
-        print(j)
-    else:
-        c.send(bytes("no","utf-8"))
-        
-    c.close()

a0f97ed23e97944c550488b95ec06fc7f8a00a49
carryfanhub <sademuditraj4422@gmail.com>
2020-06-25 01:01:23 +0300

    extra.py

--- extra.py
+++ extra.py
@@ -1,1 +1,35 @@
+#praise the lord,jesus,christ
+#lord jesus christ please help me
+x=[]
+for i in range(0,6):
+    y = str(input())
+    x.append(y)
+    
+print(x)
+v=[]
+for t in x:
+    for p in range(0,1):
+        v.append(t[p])
+        
+print(v)
+
+op=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+h=[]
+for g in v:
+    for k in range(0,26):
+        if g == op[k]:
+            h.append(k)
+            break
+        else:
+            continue
+m = []
+while True:
+    k = min(h)
+    m.append(k)
+    h.remove(k)
+    if len(h)==0:
+        break
+    
+print(m)
+
 
